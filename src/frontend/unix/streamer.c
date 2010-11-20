69b9896796bb77476e6f232d71c05ef474ceae41
Jesper Svennevid <github@svennevid.se>
2010-11-20 23:03:25 +0100

    Added a unix implementation of the streamer frontend Switched to a custom globber when collecting files to allow sharing files in the same filter

---
+++ src/frontend/unix/streamer.c
@@ -0,0 +1,135 @@
+#include "../../backend/backend.h"
+#include "../../backend/drivers/driver.h"
+
+#include <stdint.h>
+#include <pthread.h>
+#include <stdio.h>
+
+static volatile uint32_t s_shutdown = 0;
+static pthread_mutex_t s_condMutex = PTHREAD_MUTEX_INITIALIZER;
+static pthread_cond_t s_cond = PTHREAD_COND_INITIALIZER;
+static pthread_t s_thread = 0;
+
+static void internalStreamerSignal();
+
+static void* streamerThread(void* arg)
+{
+	while (!s_shutdown)
+	{
+		switch (internalStreamerIdle())
+		{
+			case StreamerResult_Pending:
+			{
+#if defined(__APPLE__)
+				pthread_yield_np();
+#else
+				pthread_yield();
+#endif
+			}
+			break;
+
+			default:
+			{
+				pthread_mutex_lock(&s_condMutex);
+				pthread_cond_wait(&s_cond, &s_condMutex);
+				pthread_mutex_unlock(&s_condMutex);
+			}
+			break;
+		}
+	}
+
+	s_shutdown = 1;
+	pthread_exit(0);
+}
+
+int streamerInitialize(StreamerTransport transport, StreamerContainer container, const char* root, const char* file)
+{
+	int ret;
+
+	if (internalStreamerInitialize(transport, container, root, file) < 0)
+	{
+		STREAMER_PRINTF(("Streamer: Failed initializing streamer engine\n"));
+		return StreamerResult_Error;
+	}
+
+	ret = pthread_create(&s_thread, 0, streamerThread, 0);
+	if (ret != 0)
+	{
+		STREAMER_PRINTF(("Streamer: Failed creating thread (%d)\n", ret));
+		return StreamerResult_Error;
+	}
+
+	return StreamerResult_Ok;
+}
+
+int streamerShutdown()
+{
+	if (s_thread)
+	{
+		s_shutdown = 1;
+		internalStreamerSignal();
+
+		pthread_join(s_thread, 0);
+
+		s_thread = 0; s_shutdown = 0;
+	}
+
+	if (internalStreamerShutdown() < 0)
+	{
+		STREAMER_PRINTF(("Streamer: Failed shutting down streamer engine\n"));
+		return StreamerResult_Error;
+	}
+
+	return StreamerResult_Ok;
+}
+
+int streamerPoll(int fd)
+{
+	return internalStreamerPoll(fd);
+}
+
+int streamerOpen(const char* filename, StreamerOpenMode mode)
+{
+	int result = internalStreamerOpen(filename, mode, StreamerCallMethod_Normal);
+	if (result >= 0)
+	{
+		internalStreamerSignal();
+	}
+	return result;
+}
+
+int streamerClose(int fd)
+{
+	int result = internalStreamerClose(fd, StreamerCallMethod_Normal);
+	if (result >= 0)
+	{
+		internalStreamerSignal();
+	}
+	return result;
+}
+
+int streamerRead(int fd, void* buffer, unsigned int length)
+{
+	int result = internalStreamerRead(fd, buffer, length, 0, 0, StreamerCallMethod_Normal);
+	if (result >= 0)
+	{
+		internalStreamerSignal();
+	}
+	return result;
+}
+
+int streamerLSeek(int fd, int offset, StreamerSeekMode whence)
+{
+	int result = internalStreamerLSeek(fd, offset, whence, StreamerCallMethod_Normal);
+	if (result >= 0)
+	{
+		internalStreamerSignal();
+	}
+	return result;
+}
+
+static void internalStreamerSignal()
+{
+	pthread_cond_signal(&s_cond);
+}
+
--- units.lua
+++ units.lua
@@ -1,3 +1,50 @@
+-- Based on FGlob, this globber supports filtering into multiple configurations
+local function MGlob(args)
+        -- Use the regular glob to fetch the file list.
+        local files = Glob(args)
+        local pats = {}
+        local result = {}
+
+        -- Construct a mapping from { Pattern = ..., Configs = ... }
+        -- to { Pattern = { { Config = ... }, ... } } with new arrays per config that can be
+        -- embedded in the source result.
+        for _, fitem in ipairs(args.Filters) do
+		local tabs = {}
+		for _, citem in ipairs(fitem.Configs) do
+                	local tab = { Config = assert(citem) }
+			tabs[#tabs + 1] = tab
+		end
+                pats[assert(fitem.Pattern)] = tabs
+                result[#result + 1] = tabs
+        end
+
+        -- Traverse all files and see if they match any configuration filters. If
+        -- they do, stick them in matching list. Otherwise, just keep them in the
+        -- main list. This has the effect of returning an array such as this:
+        -- {
+        --   { "foo.c"; Config = "abc-*-*" },
+        --   { "bar.c"; Config = "*-*-def" },
+        --   "baz.c", "qux.m"
+        -- }
+        for _, f in ipairs(files) do
+                local filtered = false
+                for filter, list in pairs(pats) do
+                        if f:match(filter) then
+                                filtered = true
+				for _, litem in ipairs(list) do
+					litem[#litem + 1] = f
+				end
+				break
+                        end
+                end
+                if not filtered then
+                        result[#result + 1] = f
+                end
+        end
+        return result
+end
+
+
 StaticLibrary
 {
 	Name = "streamer.frontend",
@@ -5,12 +52,13 @@ StaticLibrary
 
 	Sources = {
 		{
-			FGlob {
+			MGlob {
 				Dir = "src/frontend",
 				Extensions = { ".c" },
 				Filters = {
-					{ Pattern = "/win32/", Extensions = { ".c" }, Config = "win32-*-*-*" },
-					{ Pattern = "/ee/", Extensions = { ".c" }, Config = "ps2-*-*-*" }
+					{ Pattern = "/win32/", Extensions = { ".c" }, Configs = { "win32-*-*-*" } },
+					{ Pattern = "/ee/", Extensions = { ".c" }, Configs = { "ps2-*-*-*" } },
+					{ Pattern = "/unix/", Extensions = { ".c" }, Configs = { "macosx-*-*-*", "linux-*-*-*" } }
 				}
 			}
 		}
